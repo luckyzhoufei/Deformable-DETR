f7b8f5f3a6788e2c4c62c431a4011de00c75d2bd
fei.zhou <fei.zhou@ly.com>
2026-07-13 18:55:50 +0800

    read

--- models/deformable_transformer.py
+++ models/deformable_transformer.py
@@ -243,7 +243,7 @@ class DeformableTransformerEncoder(nn.Module):
                                           torch.linspace(0.5, W_ - 0.5, W_, dtype=torch.float32, device=device))
             ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * H_)
             ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * W_)
-            ref = torch.stack((ref_x, ref_y), -1)
+            ref = torch.stack((ref_x, ref_y), -1)  #增加维度了
             reference_points_list.append(ref)
         reference_points = torch.cat(reference_points_list, 1)
         reference_points = reference_points[:, :, None] * valid_ratios[:, None]
